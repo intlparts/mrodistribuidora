9c3bc96ea81bf8dc3395a98b4167c40de1b8b18c
ingacastro <ingacastro@gmail.com>
2020-10-07 15:47:00 -0400

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/jsanchez/Desktop/internationalparts/mrodistribuidora/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/jsanchez/Desktop/internationalparts/mrodistribuidora/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/acastro/Documentos/Alejandro2/mrodistribuidora/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/acastro/Documentos/Alejandro2/mrodistribuidora/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
